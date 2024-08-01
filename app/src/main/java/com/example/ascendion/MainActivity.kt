0fad62c31d41ceff65d8ff0e404ee76d8647f160
abdul <abdul.tanveer@gmail.com>
2024-08-01 11:30:55 +0530

    gettting n setting the text

--- app/src/main/java/com/example/ascendion/MainActivity.kt
+++ app/src/main/java/com/example/ascendion/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.ascendion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  nameEditText: EditText    //declaration
+    lateinit var mainTextView: TextView
+
+//componentwillmount
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.etName)  //initialization
+        mainTextView = findViewById(R.id.tvMain)
     }
+
+    //componentdidmount
+    override fun onStart() {
+        super.onStart()
+    }
+
+
+    fun clickHandler(view: View) {
+        //get the data from edittext
+        var data = nameEditText.text.toString()
+        //put it on the textview
+        mainTextView.setText(data)
+    }
+
 }
