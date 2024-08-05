d318b9491ea92d3adc0ede7e6cb5181675dab5a3
abdul <abdul.tanveer@gmail.com>
2024-08-05 15:42:54 +0530

    roomdb -1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+
+
 }
 
 android {
@@ -49,6 +52,11 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.room:room-common:2.7.0-alpha05")
+   // implementation("androidx.room:room-compiler:2.7.0-alpha05")
+    kapt ("androidx.room:room-compiler:2.6.0")
+
+    implementation("androidx.room:room-ktx:2.7.0-alpha05")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
